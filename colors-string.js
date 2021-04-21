98839f747d56b260055bff3642cde57294d7ccd8
Eng.Wesal <eng-wesal@eng.wesal-HP-250-G7-Notebook-PC>
2021-04-22 00:28:12 +0300

    switch statment with String

---
+++ colors-string.js
@@ -0,0 +1,54 @@
+
+  {
+    let color = prompt("Enter a color");
+    
+   
+
+    switch (color) {
+      case "red":
+        alert("apple");
+        break;
+      case "yellow":
+        alert("banana");
+        break;
+      case "green":
+        alert("grass");
+        break;
+      case "rectangle":
+        alert("shape");
+        break;
+      case "square":
+        alert("shape");
+        break;
+      case color.toString():
+       alert("i don't know");
+        break;
+    }
+
+  }
+
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+         
+    
+ 
+   
+ 
+ 
+    
